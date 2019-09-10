69ab2dea53411ac2250b776f239a1b674664bb7e
nogaems <nogaems@users.noreply.github.com>
2019-09-10 14:27:17 +0300

    fix: entire config
    Since I've moved to `-X` emacs build, I'm removing some redundant
    code and cleaning it up according to the latest vesrion of emacs.

--- init.el
+++ init.el
@@ -1,15 +1,14 @@
 ;; init.el --- Emacs configuration
 
-;; Make it possible to use several config files at the same time
-(add-to-list 'load-path (expand-file-name "~/.emacs.d/inits/"))
+;; Here's a temporary workaround
+(setq gnutls-algorithm-priority "NORMAL:-VERS-TLS1.3")
 
 ;; INSTALL PACKAGES
 ;; --------------------------------------
-
 (require 'package)
 
 (add-to-list 'package-archives
-       '("melpa" . "http://melpa.org/packages/") t)
+       '("melpa" . "https://melpa.org/packages/") t)
 
 (package-initialize)
 (when (not package-archive-contents)
@@ -17,11 +16,13 @@
 
 (defvar myPackages
   '(better-defaults
-    ein
+    ace-jump-mode
+    color-theme-modern
+    nasm-mode
+    ag
     elpy
     pyvenv
     flycheck
-    material-theme
     py-autopep8))
 
 (mapc #'(lambda (package)
@@ -37,25 +38,20 @@
       c-basic-offset 4)
 ;; BASIC CUSTOMIZATION
 ;; --------------------------------------
-(require 'color-theme)
-(color-theme-initialize)
-(color-theme-calm-forest)
-
+(load-theme 'material t t)
+(enable-theme 'material)
+(desktop-save-mode 1)
 (setq inhibit-startup-message t) ;; hide the startup message
-;;(load-theme 'material t) ;; load material theme
-;; Once i've moved to 26 version I don't need it anymore, so I commented it out
-;;(global-linum-mode t) ;; enable line numbers globally
+(menu-bar-mode -1)
 (when (version<= "26.0.50" emacs-version )
   (global-display-line-numbers-mode))
-
-
 (setq column-number-mode t)
 
 ;; PYTHON CONFIGURATION
 ;; --------------------------------------
 
 (elpy-enable)
-(elpy-use-ipython)
+;;(elpy-use-ipython)
 
 ;; use flycheck not flymake with elpy
 (when (require 'flycheck nil t)
@@ -65,17 +61,6 @@
 ;; enable autopep8 formatting on save
 (require 'py-autopep8)
 (add-hook 'elpy-mode-hook 'py-autopep8-enable-on-save)
-
-;; To support shortcuts in Russian keyboard layout
-
-(loop
- for from across "йцукенгшщзхъфывапролджэячсмитьбюЙЦУКЕНГШЩЗХЪФЫВАПРОЛДЖ\ЭЯЧСМИТЬБЮ№"
- for to   across "qwertyuiop[]asdfghjkl;'zxcvbnm,.QWERTYUIOP{}ASDFGHJKL:\"ZXCVBNM<>#"
- do
- (eval `(define-key key-translation-map (kbd ,(concat "C-" (string from))) (kbd ,(concat
-"C-" (string to)))))
- (eval `(define-key key-translation-map (kbd ,(concat "M-" (string from))) (kbd ,(concat
-"M-" (string to))))))
 
 ;; Add ace-jump-mode
 
@@ -89,99 +74,13 @@
 ;; ag
 (require 'ag)
 
-;; add session saving during exit and restoring after restart
-;; use only one desktop
+;; session saving across restarts
 (setq desktop-path '("~/.emacs.d/"))
 (setq desktop-dirname "~/.emacs.d/")
 (setq desktop-base-file-name "emacs-desktop")
-(setq desktop-save-mode 0)
-(setq desktop-restore-frames nil)
-(setq desktop-load-locked-desktop t)
-
-(defun saved-session ()
-  (file-exists-p (concat desktop-dirname "/" desktop-base-file-name)))
-
-;; use session-restore to restore the desktop manually
-(defun session-restore ()
-  "Restore a saved emacs session."
-  (interactive)
-  (if (saved-session)
-      (desktop-read)
-    (message "No desktop found.")))
-
-;; use session-save to save the desktop manually
-(defun session-save ()
-  "Save an emacs session."
-  (interactive)
-  (if (saved-session)
-      (if (y-or-n-p "Overwrite existing desktop? ")
-	  (desktop-save-in-desktop-dir)
-	(message "Session not saved."))
-  (desktop-save-in-desktop-dir)))
-
-(add-hook 'kill-emacs-query-functions 'desktop-save-in-desktop-dir)
-
-;; Quicklisp
-(if (file-exists-p "~/quicklisp/dists/quicklisp/software/")
-    (let ((slime-path
-           (car (file-expand-wildcards
-                 (expand-file-name "~/quicklisp/dists/quicklisp/software/slime-*")))))
-      (if (file-exists-p slime-path) (add-to-list 'load-path slime-path))))
-
-;; Slime
-
-  (load (expand-file-name "~/quicklisp/slime-helper.el"))
-  ;; Replace "sbcl" with the path to your implementation
-  (setq inferior-lisp-program "sbcl")
 
 ;; Delelte trailing whitespaces in every mode
 (add-hook 'before-save-hook 'delete-trailing-whitespace)
-
-;; Slime
-;;(require 'slime)
-;(add-hook 'lisp-mode-hook (lambda () (slime-mode t)))
-;;(add-hook 'inferior-lisp-mode-hook (lambda () (inferior-slime-mode t)))
-;; Optionally, specify the lisp program you are using. Default is "lisp"
-;;(setq inferior-lisp-program "yourlisp")
-
-
-;; Org mode
-(setq org-directory "~/.emacs.d/org/")
-;; Don't forget to do that after setting this path
-;; cd ~/.emacs.d/org ; touch touch todo.org diary.org org.org bookmarks.org refile.org work.org events.org paste.org
-(require 'init-org)
-;; Hotkeys
-(define-prefix-command 'org-menu-map)
-(define-key global-map (kbd "<f9>") 'org-menu-map)
-(define-key global-map (kbd "<f9> <f9>") 'bh/show-org-agenda)
-(define-key global-map (kbd "<f9> b") 'bbdb)
-(define-key global-map (kbd "<f9> c") 'calendar)
-(define-key global-map (kbd "<f9> f") 'boxquote-insert-file)
-(define-key global-map (kbd "<f9> g") 'gnus)
-(define-key global-map (kbd "<f9> h") 'bh/hide-other)
-(define-key global-map (kbd "<f9> n") 'org-narrow-to-subtree)
-(define-key global-map (kbd "<f9> u") 'bh/narrow-up-one-level)
-(define-key global-map (kbd "<f9> o") 'bh/make-org-scratch)
-(define-key global-map (kbd "<f9> s") 'bh/switch-to-scratch)
-(define-key global-map (kbd "<f9> w") 'widen)
-(define-key global-map (kbd "<f9> r") 'boxquote-region)
-(define-key global-map (kbd "<f9> T") 'tabify)
-(define-key global-map (kbd "<f9> U") 'untabify)
-(define-key global-map (kbd "<f9> v") 'visible-mode)
-(define-key global-map (kbd "<f7>") 'bh/set-truncate-lines)
-(define-key global-map (kbd "<f5>") 'bh/org-todo)
-(define-key global-map (kbd "S-<f5>") 'bh/widen)
-(define-key global-map (kbd "C-c l") 'org-store-link)
-(define-key global-map (kbd "C-c a") 'org-agenda)
-(define-key global-map (kbd "C-c b") 'org-iswitchb)
-(define-key global-map (kbd "C-x m") 'browse-url-at-point)
-(define-key global-map (kbd "<f8>") 'org-cycle-agenda-files)
-(define-key global-map (kbd "C-<f9>") 'previous-buffer)
-(define-key global-map (kbd "M-<f9>") 'org-toggle-inline-images)
-(define-key global-map (kbd "C-<f10>") 'next-buffer)
-(define-key global-map (kbd "<f11>") 'org-clock-goto)
-(define-key global-map (kbd "C-<f11>") 'org-clock-in)
-(define-key global-map (kbd "C-c c") 'org-capture)
 
 ;; fix bindings
 
@@ -228,25 +127,27 @@
 (define-key function-key-map "\e[5;5~" [C-prior])
 (define-key function-key-map "\e[6;5~" [C-next])
 
-;(define-key function-key-map "\e[2;3~" [M-insert])
-;(define-key function-key-map "\e[3;3~" [M-delete])
-;(define-key function-key-map "\e\e[7~" [M-home])
-;(define-key function-key-map "\e\e[8~" [M-end])
-;(define-key function-key-map "\e[5;3~" [M-prior])
-;(define-key function-key-map "\e[6;3~" [M-next])
-
-;(define-key function-key-map "\e[2;7~" [C-M-insert])
-;(define-key function-key-map "\e[3;7~" [C-M-delete])
-;(define-key function-key-map "\e[1;7H" [C-M-home])
-;(define-key function-key-map "\e[1;7F" [C-M-end])
-;(define-key function-key-map "\e[5;7~" [C-M-prior])
-;(define-key function-key-map "\e[6;7~" [C-M-next])
-
-;; Set palemoon as a default browser
+;; Set midori as default browser
 (setq browse-url-browser-function 'browse-url-generic
-      browse-url-generic-program "palemoon")
+      browse-url-generic-program "midori")
 
 ;; Start the server
-(server-start)
+;; Currently not running it in this mode, commented out
+;;(server-start)
 
 ;; init.el ends here
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   (quote
+    (color-theme-modern py-autopep8 nasm-mode material-theme flycheck elpy ein better-defaults ag ace-jump-mode))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
